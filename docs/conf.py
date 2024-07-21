4c76d6bb13e7e6a1a54dbbba0ce765b65eafdf4a
jiahaoli57 <jiahaoli57@163.com>
2024-07-22 01:09:04 +0800

    build docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_static_path = ['_static']
