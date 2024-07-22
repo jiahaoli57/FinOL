b1eecfe33c74928083e98d3d2a56e9ff1f089b6d
jiahaoli57 <jiahaoli57@163.com>
2024-07-22 12:52:04 +0800

    build docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,3 +27,12 @@ exclude_patterns = []
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"  # 'alabaster'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]  # ['_static']
+
+
+
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
