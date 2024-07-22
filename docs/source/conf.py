29e17f6ce4de8908358346644b2d52d336207b6d
jiahaoli57 <jiahaoli57@163.com>
2024-07-22 13:47:01 +0800

    build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ release = 'MIT'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["recommonmark", "sphinx_markdown_tables", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,7 +28,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"  #'alabaster'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"  # 'alabaster'
 # html_static_path = ['_static']
