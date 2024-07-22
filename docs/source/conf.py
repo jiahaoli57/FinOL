9d385b4d895b25832d433ffa1577865e92582b6b
jiahaoli57 <jiahaoli57@163.com>
2024-07-22 13:45:02 +0800

    build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,15 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
+from datetime import datetime
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'FinOL'
-copyright = '2024, FinOL contributors'
+# copyright = f'2024–{datetime.now().year}, FinOL contributors'
+copyright = f'2024, FinOL contributors'
 author = 'FinOL contributors'
 release = 'MIT'
 
@@ -24,5 +28,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"  #'alabaster'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_static_path = ['_static']
