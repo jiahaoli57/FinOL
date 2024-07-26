795a11fa3bedc65d37d812c4621e1ce21320dd77
jiahaoli57 <jiahaoli57@163.com>
2024-07-27 01:21:46 +0800

    build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,9 +5,11 @@
 
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../finol'))
+sys.path.insert(0, os.path.abspath('../../finol/data_layer'))
 # import finol
 # import warnings
 # import plotly.io as pio
