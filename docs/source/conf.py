6b5b42a5b597b3414e34113b016c419d687c210d
jiahaoli57 <jiahaoli57@163.com>
2024-07-27 02:51:13 +0800

    build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 sys.path.insert(0, os.path.abspath('../../finol'))
 sys.path.insert(0, os.path.abspath('../../finol/data_layer'))
 sys.path.insert(0, os.path.abspath('../../finol/data_layer/'))
