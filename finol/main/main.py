d63897b927ff131d21d3a5f5a1038a14a2c407b3
jiahaoli57 <jiahaoli57@163.com>
2024-08-08 15:57:49 +0800

    build docs; optimize code

--- finol/main/main.py
+++ finol/main/main.py
@@ -2,7 +2,6 @@ from finol.data_layer.dataset_loader import DatasetLoader
 from finol.optimization_layer.model_trainer import ModelTrainer
 from finol.evaluation_layer.model_evaluator import ModelEvaluator
 
-
 load_dataset_output = DatasetLoader().load_dataset()
 train_model_output = ModelTrainer(load_dataset_output).train_model()
 evaluate_model_output = ModelEvaluator(load_dataset_output, train_model_output).evaluate_model()
