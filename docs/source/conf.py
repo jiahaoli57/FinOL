3c51ec24c3dcd9b06727cabd48307b980d26e1c6
jiahaoli57 <jiahaoli57@163.com>
2024-08-12 12:15:21 +0800

    build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,7 +72,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # The name of the Pygments (syntax highlighting) style to use.
